0b53e5a9490621cd4c123917d95efcd42d6b3eb6
KouyaFukuda <kouyaf77@gmail.com>
2017-02-14 17:13:38 -0300

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 
+# Create symbolic link
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+# Install dein
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh installer.sh ~/dotfiles/.vim/dein
+rm installer.sh
